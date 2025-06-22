29fb1979e124830b1399f50dc736fc5b50c08461
= <=>
2025-06-22 14:41:02 +0200

    dodata mapa

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import MapComponent from "./MapComponent/MapComponent";
 
 function App() {
-	return <div>Hello World</div>;
+	return (
+		<div>
+			<Header />
+			<MapComponent />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/MapComponent/MarkerLocations.jsx
@@ -0,0 +1,14 @@
+export const MarkerLocations = [
+	{
+		location: [43.14163535508256, 20.51813593460599],
+		popUp: "Muzej Ras",
+	},
+	{
+		location: [43.141031073656954, 20.517888175811795],
+		popUp: "Rostilj kod Jonuza",
+	},
+	{
+		location: [43.140851580653376, 20.518408411726277],
+		popUp: "Kulturni centar NP",
+	},
+];
