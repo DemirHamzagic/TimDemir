e0b3bb87ce8962d235a804677c4b40540cbda4e6
saraslavkovic <slavkovic.sara2009@gmail.com>
2025-07-05 12:39:41 +0200

    gotova pocetna stranica

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LocationDetails from "./MapComponent/LocationDetails";
-import MapComponent from "./MapComponent/MapComponent";
-import Onama from "./pages/Onama";
-import Contact from "./pages/Kontakt";
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LocationDetails from './MapComponent/LocationDetails';
+import MapComponent from './MapComponent/MapComponent';
+import Onama from './pages/Onama';
+import Contact from './pages/Kontakt';
+import Pocetna from './pages/Pocetna';
 
 function App() {
-	return (
-		<BrowserRouter>
-			<LocationDetails />
-			<Header />
-			<Routes>
-				<Route path="/" element={<MapComponent />} />
-				<Route path="/Onama" element={<Onama />} />
-				<Route path="/kontakt" element={<Contact />} />
-			</Routes>
-			<Footer />
-		</BrowserRouter>
-	);
+    return (
+        <BrowserRouter>
+            <LocationDetails />
+            <Header />
+            <Routes>
+                <Route path="/" element={<Pocetna />} />
+                <Route path="/Onama" element={<Onama />} />
+                <Route path="/kontakt" element={<Contact />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+    );
 }
 
 export default App;
